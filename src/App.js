a87f74e78e86e20bb64a5d5b56bc0245aca45ab7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-09 19:01:33 -0800

    count usestate

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hello from "./component/Hello";
 import Welcome from "./component/Welcome";
 import Title from "./component/Title";
 import Myname from "./component/Myname";
+import Mycount from "./component/Mycount";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Title name="Mobile Developer John" />
       <Title name="Solodity  Developer John" />
       <Myname/>
+      <Mycount/>
     </div>
   );
 }
